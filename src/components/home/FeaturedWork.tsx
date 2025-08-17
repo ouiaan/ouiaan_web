714e92be815d941b59af92632445efbfd22bd65a
Iaan <iaanjfr@gmail.com>
2025-08-17 19:51:17 +0000

    ok, olvida todo lo anterior, hagamos algo diferente con el carusel, las

--- src/components/home/FeaturedWork.tsx
+++ src/components/home/FeaturedWork.tsx
@@ -42,6 +42,7 @@ export function FeaturedWork() {
             opts={{
               align: "start",
               loop: true,
+              dragFree: true,
             }}
             className="w-full cursor-grab active:cursor-grabbing"
           >
